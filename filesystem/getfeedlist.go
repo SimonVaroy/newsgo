6fad1c9b1a2111f6d481979b70cdefa94386cb64
Simon Nikolai Varøy <simon@simonvaroy.xyz>
2023-06-10 23:40:46 +0200

    moved the previous function to another dir/pkg

--- filesystem/getfeedlist.go
+++ filesystem/getfeedlist.go
@@ -2,8 +2,21 @@ package filesystem
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func GetFeedList() {
-	fmt.Println("Here are my rss links")
+func ReadFeedFile() ([]string, error) {
+	homeDir := os.Getenv("HOME")
+	filePath := filepath.Join(homeDir, ".config", "newsgo", "feeds")
+
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading feeds file: %s", err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	return lines, nil
 }
--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 
+	"github.com/SimonVaroy/newsgo/filesystem"
 	"github.com/SimonVaroy/newsgo/rss"
 )
 
 func main() {
-	feedLines, err := rss.ReadFeedFile()
+	feedLines, err := filesystem.ReadFeedFile()
 	if err != nil {
 		fmt.Println("Error reading feed lines:", err)
 		return
 	}
 
 	fmt.Println(feedLines)
+	rss.FetchRss("https://lukesmith.xyz/index.xml")
 }
